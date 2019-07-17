03ca2666364b476c0a9255e41cd7c1079e498394
Carson Wack <carson_wack@yahoo.com>
2019-07-17 09:31:19 -0700

    setup

--- src/App.js
+++ src/App.js
@@ -1,10 +1,34 @@
 import React from 'react';
 import './App.css';
+import passport from 'passport';
+import dotenv from 'dotenv';
+dotenv.config();
+
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+
+
+const GOOGLE_CLIENT_ID = process.env.GOOGLE_CLIENT_ID;
+const GOOGLE_CLIENT_SECRET = process.env.GOOGLE_CLIENT_SECRET;
+
+
+// Configure Passport to use the Google OAth 2.0 strategy
+
+// passport.use(new GoogleStrategy({
+//   clientID: GOOGLE_CLIENT_ID,
+//   clientSecret: GOOGLE_CLIENT_SECRET,
+//   callbackURL: "http://www.example.com/auth/google/callback"
+// },
+//   function (accessToken, refreshToken, profile, cb) {
+//     User.findOrCreate({ googleId: profile.id }, function (err, user) {
+//       return cb(err, user);
+//     });
+//   }
+// ));
 
 function App() {
-  return ( 
-    null
-     );
+  return (
+    <h1>hi</h1>
+  );
 }
 
 export default App;
