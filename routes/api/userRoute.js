6ae5f44b332116b666c397b2f4b5911e882ce16e
Carson Wack <carson_wack@yahoo.com>
2019-07-19 12:36:19 -0700

    rearrange

---
+++ routes/api/userRoute.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const userController = require('../../controllers/userController');
+
+const db = require('../../models');
+
+module.exports = function (app) {
+
+    
+
+
+}
+
+
